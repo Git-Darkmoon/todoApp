87a8eb50e838047ebe4febfe861f881a9ec97b0c
Nicolas Mayorga <namayorgav@gmail.com>
2022-09-08 22:07:51 -0500

    Little progress

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const todoContainer = document.querySelector(".myUl")
 const userInput = document.getElementById("userInput")
 const inputBtn = document.getElementById("inputBtn")
+const checkInput = document.querySelectorAll("#taskCheck")
+
+let todoCount = 0
 
 let newTodo = document.createElement("li")
 newTodo.classList.add("eachTodo")
@@ -11,7 +14,8 @@ inputBtn.addEventListener("click", (e) => {
     let newTodo = document.createElement("li")
     newTodo.classList.add("eachTodo")
     newTodo.innerHTML = `
-    <div><input type="checkbox" name="" id="">${userInput.value.trim()}</input></div>
+    <input type="checkbox" name="" id="taskCheck" />
+    <label for="taskCheck">${userInput.value.trim()}</label>
     <div><button>X</button></div>
 `
     todoContainer.appendChild(newTodo)
@@ -19,4 +23,16 @@ inputBtn.addEventListener("click", (e) => {
     alert("Must enter a task to add !")
   }
   userInput.value = ""
+})
+
+checkInput.forEach((liInput) => {
+  liInput.addEventListener("change", (e) => {
+    if (e.currentTarget.checked == true) {
+      console.log("Cool")
+      liInput.nextElementSibling.style.textDecoration =
+        "line-through solid #00b32599 35%"
+    } else {
+      liInput.nextElementSibling.style.textDecoration = "none"
+    }
+  })
 })
